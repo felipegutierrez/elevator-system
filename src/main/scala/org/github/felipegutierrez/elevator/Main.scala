66543abc70a09b3c46f3c8d26b48d0c239461dae
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-02-05 09:27:48 +0100

    First object

---
+++ src/main/scala/org/github/felipegutierrez/elevator/Main.scala
@@ -0,0 +1,10 @@
+package org.github.felipegutierrez.elevator
+
+/**
+ * This is a control system for elevators.
+ */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(s"This is a control system for elevators")
+  }
+}
